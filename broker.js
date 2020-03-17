a5d8da032c0b7a556ecd028c3c46f39e37a33c88
Alfredo <josalfk@hotmail.com>
2020-03-16 21:32:29 -0600

    changed port

--- broker.js
+++ broker.js
@@ -1,7 +1,10 @@
 const aedes = require('aedes')()
-const server = require('net').createServer(aedes.handle)
-const port = 2020
+const httpServer = require('http').createServer()
+const ws = require('websocket-stream')
+const port = 8080
 
-server.listen(port, function () {
-  console.log('server started and listening on port ', port)
+ws.createServer({ server: httpServer }, aedes.handle)
+
+httpServer.listen(port, function () {
+  console.log('websocket server listening on port ', port)
 })
