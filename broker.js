d007b05fb007ec0b3a3292dc5ca33d745e6ff298
Alfredo <josalfk@hotmail.com>
2020-03-16 22:19:52 -0600

    ws

--- broker.js
+++ broker.js
@@ -1,7 +1,9 @@
 const aedes = require('aedes')()
-const server = require('net').createServer(aedes.handle)
-const port = 8080
+const httpServer = require('http').createServer()
+const ws = require('websocket-stream')
 
-server.listen(port, function () {
-  console.log('server started and listening on port ', port)
+ws.createServer({ server: httpServer }, aedes.handle)
+
+httpServer.listen(process.env.PORT || 8080, function () {
+  console.log('websocket server listening on port ', port)
 })
