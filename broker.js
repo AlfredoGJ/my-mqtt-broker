9529dbd103602f64dbad8496227f3e8584542c8d
Alfredo <josalfk@hotmail.com>
2020-03-16 22:22:17 -0600

    ss

--- broker.js
+++ broker.js
@@ -1,9 +1,9 @@
 const aedes = require('aedes')()
 const httpServer = require('http').createServer()
 const ws = require('websocket-stream')
-
+port = process.env.PORT || 8080
 ws.createServer({ server: httpServer }, aedes.handle)
 
-httpServer.listen(process.env.PORT || 8080, function () {
+httpServer.listen(port, function () {
   console.log('websocket server listening on port ', port)
 })
